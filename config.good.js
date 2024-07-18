985f06a58f1e28e710e31f5bf79c049b23749985
Putra298 <seftyandrap@gmail.com>
2024-07-18 19:52:20 +0700

    Add files via upload

---
+++ config.good.js
@@ -0,0 +1,36 @@
+var config = {
+    name: 'vision_inverse_1',
+    width: 30,
+    height: 30,
+    scale: 20,
+    speed: 0,
+    demo:500,
+    train:200000,
+    goal: 800,
+    borders:true,
+
+    creature: {
+        type: 3,
+        count: 500,
+        energy: 30,
+        laziness: 10,
+        vision: 10
+
+    },
+
+    food: {
+        type: 2,
+        count:20,
+        energy: 30
+    },
+
+    wall: {
+        type:1,
+        count:50
+    },
+
+    brain:{
+        structure: [12,4],
+        change: 0.1
+    }
+};
---
+++ readme.md
@@ -0,0 +1,3 @@
+My first javascript AI library trains little squares to avoid walls eat food circles and explore world
+
+![alt text](./doc/screen.gif)
